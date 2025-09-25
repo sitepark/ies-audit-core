bcdbb6f0bc9ab452b6f16f1ce6b554c58d364251
Holger Veltrup <veltrup@sitepark.com>
2025-09-25 10:33:15 +0200

    refactor: use DomainException

--- src/main/java/com/sitepark/ies/audit/core/domain/exception/RevertFailedException.java
+++ src/main/java/com/sitepark/ies/audit/core/domain/exception/RevertFailedException.java
@@ -1,9 +1,10 @@
 package com.sitepark.ies.audit.core.domain.exception;
 
+import com.sitepark.ies.sharedkernel.domain.DomainException;
 import java.io.Serial;
 
 /** Exception that is thrown when an audit revert operation fails. */
-public abstract class RevertFailedException extends AuditException {
+public abstract class RevertFailedException extends DomainException {
 
   @Serial private static final long serialVersionUID = 1L;
 
